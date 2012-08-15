aac6ae64854d2f83406b4fc6ad5ece8a71132103
Ori155 <ori155@gmail.com>
2012-08-15 15:05:33 +0300

    fixed the "new-Line" bug

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=0; i < 4; i++)
-			System.out.print("Hello World");
+			System.out.println("Hello World");
 	}
 
 }
