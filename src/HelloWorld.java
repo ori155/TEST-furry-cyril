b8fe601ae77cb8c710946ecfa5a1edaccd7f315b
Ori155 <ori155@gmail.com>
2012-08-15 15:11:30 +0300

    changed the times of printing "Hello world" to 3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=0; i < 4; i++)
+		for(int i=0; i < 3; i++)
 			System.out.println("Hello World");
 	}
 
