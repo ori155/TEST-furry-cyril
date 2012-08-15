42761cc72e5e05a73a88546fd6a23e6f8762d368
Ori155 <ori155@gmail.com>
2012-08-15 15:04:47 +0300

    added loop

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,8 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello World");
+		for(int i=0; i < 4; i++)
+			System.out.print("Hello World");
 	}
 
 }
